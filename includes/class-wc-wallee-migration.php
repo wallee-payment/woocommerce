c3df599c53f3154e09c762ebf99c1076b00b2f8f
Victor Ndambakuwa <victor.ndambakuwa@wallee.com>
2021-08-02 15:09:59 +0200

    Release 1.5.8

--- includes/class-wc-wallee-migration.php
+++ includes/class-wc-wallee-migration.php
@@ -258,7 +258,7 @@ class WC_Wallee_Migration {
 	public static function plugin_row_meta( $links, $file ) {
 	    if ( WC_WALLEE_PLUGIN_BASENAME === $file ) {
 	        $row_meta = array(
-	            'docs' => '<a href="https://plugin-documentation.wallee.com/wallee-payment/woocommerce/1.5.7/docs/en/documentation.html" aria-label="' . esc_attr__('View Documentation', 'woo-wallee') . '">' . esc_html__('Documentation', 'woo-wallee') . '</a>',
+	            'docs' => '<a href="https://plugin-documentation.wallee.com/wallee-payment/woocommerce/1.5.8/docs/en/documentation.html" aria-label="' . esc_attr__('View Documentation', 'woo-wallee') . '">' . esc_html__('Documentation', 'woo-wallee') . '</a>',
 	        );
 	        
 	        return array_merge( $links, $row_meta );
